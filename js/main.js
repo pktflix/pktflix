591cc5849ad3b3416ef3a99128b127df1cc1e97b
pktflix <pktpolash@gmail.com>
2025-06-09 15:44:09 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,38 @@
-import { initClock } from './utils.js';
-import { initSlider } from './slider.js';
-
-initClock('#clock');
-
-fetch('banners.json')
+fetch('assets/banner.json')
   .then(res => res.json())
-  .then(data => initSlider('#slider', data, 3000));
+  .then(banners => {
+    const bannerSlider = document.getElementById('bannerSlider');
+    banners.forEach(banner => {
+      const a = document.createElement('a');
+      a.href = banner.link;
+      a.target = "_blank";
+      a.innerHTML = `<img src="${banner.image}" alt="${banner.title}" />`;
+      bannerSlider.appendChild(a);
+    });
+  });
 
-fetch('videos.json')
+fetch('assets/movies.json')
   .then(res => res.json())
   .then(data => {
-    const movies = data.filter(v => v.type === 'movie');
-    const series = data.filter(v => v.type === 'series');
-    const render = (list, id) => {
-      const container = document.getElementById(id);
-      list.forEach(item => {
-        const card = document.createElement('div');
-        card.className = 'card';
-        card.innerHTML = `<img src="${item.thumb}"><div class="title">${item.title}</div>`;
-        card.onclick = () => location.href = `video.html?id=${item.id}`;
-        container.append(card);
-      });
-    };
-    render(movies, 'movies');
-    render(series, 'series');
+    const movies = data.filter(item => item.type === 'movie').slice(0, 20);
+    const series = data.filter(item => item.type === 'series').slice(0, 10);
+
+    const movieList = document.getElementById('movieList');
+    const seriesList = document.getElementById('seriesList');
+
+    movies.forEach(video => {
+      movieList.innerHTML += `
+        <a href="watch.html?id=${video.id}" class="video-card">
+          <img src="${video.thumbnail}" alt="${video.title}" />
+          <h3>${video.title}</h3>
+        </a>`;
+    });
+
+    series.forEach(video => {
+      seriesList.innerHTML += `
+        <a href="watch.html?id=${video.id}" class="video-card">
+          <img src="${video.thumbnail}" alt="${video.title}" />
+          <h3>${video.title}</h3>
+        </a>`;
+    });
   });
