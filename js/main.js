9a59450c273f7d95b2427ce434fb80f4682a22e3
pktflix <pktpolash@gmail.com>
2025-06-22 23:25:22 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ fetch('tv.json')
     });
   });
 
-fetch('vide.json')
+fetch('videos.json')
   .then(res => res.json())
   .then(data => {
     const movies = data.filter(item => item.type === 'movie').slice(0, 20);
